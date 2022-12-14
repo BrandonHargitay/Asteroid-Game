ac6b5123d450826691e160656b2279d475dc1da6
Brandon Hargitay <93677930+BrandonHargitay@users.noreply.github.com>
2022-12-13 19:38:57 -0800

    Add new textures

--- Texture.cpp
+++ Texture.cpp
@@ -12,8 +12,11 @@ void Texture::load(textureEnum texture) {
 std::string Texture::getPath(textureEnum texture) {
     switch(texture){
         case ROCKET: return "Images/Rocket.png";
+        case ROCKET2: return "Images/Main Ship - Base - Full health.png";
         case ASTEROIDS: return "Images/asteriods.png";
+        case ASTEROIDPIXEL: return "Images/Baren.png";
         case BULLET: return "Images/Bullet.png";
+        case BACKGROUND: return "Images/Space_Stars2.png";
         case HEART: return  "Images/Heart.png";
         case LAST_TEXTURE: return "";
     }
